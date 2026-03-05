4531403ffafcf87daa00ddefb00095c785ab754a
mariovisnjic <mario.visnjic93@gmail.com>
2026-03-05 21:46:21 +0100

    Remove unused `Folder` and `FolderGit2` imports from components

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -1,5 +1,5 @@
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
+import { BookOpen, LayoutGrid, Menu, Search } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
 import AppLogoIcon from '@/components/app-logo-icon';
 import { Breadcrumbs } from '@/components/breadcrumbs';
